f7d141160bd6ad92d31be06c0f8a1fd764c5ca9b
Jacob Harrington <jharrington4@gmail.com>
2019-11-18 13:53:10 -0800

    created news fetch for top headlines

--- server.js
+++ server.js
@@ -1,28 +1,44 @@
 // Load Environment Variables from the .env file
-
+require('dotenv').config();
 
 // Application Dependencies
 const express = require('express');
-// (add cors, pg, and morgan...)
+const cors = require('cors');
+const morgan = require('morgan');
+const superagent = require('superagent');
 
 // Database Client
 // (create and connect using DATABASE_URL)
-
+const client = require('./lib/client');
+client.connect();
 
 // Application Setup
 const app = express();
-// (add middleware utils: logging, cors, static files from public)
-// app.use(...)
+const PORT = process.env.PORT;
+const NEWS_API_KEY = process.env.NEWS_API_KEY;
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.json());
 
 
 // API Routes
+app.get('api/news', async (req, res) => {
+    try {
+        let news = await superagent.get('https://newsapi.org/v2/top-headlines?language=en').set(`X-Api-Key`, `${NEWS_API_KEY}`);
+        res.status(200).json(news.articles);
+    }
+    catch (err){
+        res.status(500).json(err);
+        console.log(err);
+    }
+});
 
 // http method and path...
 
 
 // Start the server
 // (use PORT from .env!)
-const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log('server running on PORT', PORT);
