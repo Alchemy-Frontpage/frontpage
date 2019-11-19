77d4006db31d6e1525b98d4ed59bfc76aeb2e49d
Eve Stockstill <evestockstillpdx@gmail.com>
2019-11-18 17:04:01 -0800

    test auth

--- server.js
+++ server.js
@@ -11,6 +11,36 @@ const superagent = require('superagent');
 // (create and connect using DATABASE_URL)
 const client = require('./lib/client');
 client.connect();
+const ensureAuth = require('./lib/auth/ensure-auth');
+const createAuthRoutes = require('./lib/auth/create-auth-routes');
+const authRoutes = createAuthRoutes({
+    selectUser(email) {
+        return client
+            .query(
+                `
+            SELECT id, email, hash 
+            FROM users
+            WHERE email = $1;
+        `,
+                [email]
+            )
+            .then(result => result.rows[0]);
+    },
+    insertUser(user, hash) {
+        console.log(user);
+        return client
+            .query(
+                `
+            INSERT into users (email, hash)
+            VALUES ($1, $2)
+            RETURNING id, email;
+        `,
+                [user.email, hash]
+            )
+            .then(result => result.rows[0]);
+    }
+});
+
 
 // Application Setup
 const app = express();
@@ -20,6 +50,8 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
+app.use('/api/auth', authRoutes);
+app.use('/api', ensureAuth);
 
 
 // API Routes
