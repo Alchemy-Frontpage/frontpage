ac61f89e955b648383bbf8e3512caab3c8e9560c
JMoneymaker <jasmin.moneymaker@gmail.com>
2019-11-20 13:45:17 -0800

    pulling from dev

--- public/favPage/FavPageApp.js
+++ public/favPage/FavPageApp.js
@@ -1,7 +1,7 @@
 import Component from '../Component.js';
 import Header from '../common/Header.js';
 import Footer from '../common/Footer.js';
-import FilterBar from '../common/FilterBar.js';
+// import FilterBar from '../common/FilterBar.js';
 import FavoriteList from './FavoriteList.js';
 import KeywordFilter from './KeywordFilter.js';
 import { deleteFavorite, getFavorites, getFavoritesFilter } from '../services/domain-api.js';
@@ -10,25 +10,6 @@ class FavPageApp extends Component {
     async onRender(dom) {
         const header = new Header();
         dom.prepend(header.renderDOM());
-
-            
-        const filterBar = new FilterBar();
-        dom.append(filterBar.renderDOM());
-
-        const favoriteList = new FavoriteList({
-            articles: [],
-            onDelete: async favoriteToRemove => {
-                try {
-                    await deleteFavorite(favoriteToRemove.id);
-                    let articles = await getFavorites();
-                    favoriteList.update({ articles });
-                }
-                catch (err){
-                    console.log(err);
-                }
-            }
-        });
-        dom.appendChild(favoriteList.renderDOM());
 
         const keywordFilter = new KeywordFilter({ 
             onSubmit: async (searchQuery) => {
@@ -52,6 +33,27 @@ class FavPageApp extends Component {
 
         const footer = new Footer();
         dom.append(footer.renderDOM());
+
+            
+        // const filterBar = new FilterBar();
+        // dom.append(filterBar.renderDOM());
+
+        const favoriteList = new FavoriteList({
+            articles: [],
+            onDelete: async favoriteToRemove => {
+                try {
+                    await deleteFavorite(favoriteToRemove.id);
+                    let articles = await getFavorites();
+                    favoriteList.update({ articles });
+                }
+                catch (err){
+                    console.log(err);
+                }
+            }
+        });
+        dom.appendChild(favoriteList.renderDOM());
+
+        
 
     }
     renderHTML(){
